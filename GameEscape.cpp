e2b56c0776746f3b2719f8ec8668dfb55982f20e
DarkSky3309 <lust3309@gmail.com>
2022-01-29 14:37:10 +0300

    first commit

---
+++ GameEscape.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GameEscape.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameEscape, "GameEscape" );
---
+++ GameEscapeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameEscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEESCAPE_API AGameEscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
